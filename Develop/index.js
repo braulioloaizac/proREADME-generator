daf5a38eca89b3c36491d88dbc49ea633f6a7e58
braulioloaizac <bloaizac@uqvirtual.edu.co>
2022-07-02 14:30:09 -0400

    created the template and added questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,54 @@
-// TODO: Include packages needed for this application
+import inquirer from "inquirer";
+import fs from "fs";
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your project title?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a short description explaining the what, why, and how of your project.'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide a step-by-step description of how to get the development environment running. (Installation instructions)'
+        
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use.'
+        
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'List your collaborators, if any, with links to their GitHub profiles.'
+    },
+    {
+        type: 'checkbox',
+        name: 'licence',
+        message: 'List your collaborators, if any, with links to their GitHub profiles.',
+        choices: ['GNU AGPLv3','GNU GPLv3','GNU LGPLv3','Mozilla Public License 2.0','Apache License 2.0','MIT License','Boost Software License 1.0','The Unlicense']
+    },
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+];
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {
+    // inquirer.prompt(questions).then((answers) => {
+//     console.log(JSON.stringify(answers, null, '  '));
+//   });
+    
+//}
 
-// Function call to initialize app
-init();
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
+
---
+++ Develop/pro-template.js
@@ -0,0 +1,51 @@
+const generateREADME = () =>{
+
+    return`
+    # <Your-Project-Title>
+
+    ## Description
+
+    
+
+    ## Table of Contents (Optional)
+
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Credits](#credits)
+    - [License](#license)
+
+    ## Installation
+
+
+    ## Usage
+
+     Include screenshots as needed.
+
+
+    ## Credits
+
+
+    ## License
+
+
+    ## Badges
+
+    ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+
+    Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+
+    ## Features
+
+    If your project has a lot of features, list them here.
+
+    ## How to Contribute
+
+    If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+
+    ## Tests
+
+    Go the extra mile and write tests for your application. Then provide examples on how to run them here.
+`
+}
+
+export default generateREADME;
